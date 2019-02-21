45bcd255eebcd46e7611e9917ddc419000dcb841
lujiachao <632849093@qq.com>
2019-02-21 15:47:08 +0800

    简单使用

--- RSAstudy/RSAHelper.cs
+++ RSAstudy/RSAHelper.cs
@@ -1,11 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using System.Text;
+using XC.RSAUtil;
 
 namespace RSAstudy
 {
     public class RSAHelper
     {
+        private static RSAUtilBase _RSAUtilBase;
+
+        public RSAHelper(RSAUtilBase rSAUtilBase)
+        {
+            _RSAUtilBase = rSAUtilBase;
+        }
+
+        public static string RSAEncryupt(RSAUtilBase rSAUtilBase, RSAEncryptionPadding rSAEncryptionPadding, string data) => _RSAUtilBase.Encrypt(data, rSAEncryptionPadding);
+
+        public static string RSADecrypt(RSAUtilBase rSAUtilBase, RSAEncryptionPadding rSAEncryptionPadding, string data) => _RSAUtilBase.Decrypt(data, rSAEncryptionPadding);
+
+        public static string RSASignData(RSAUtilBase rSAUtilBase, HashAlgorithmName hashAlgorithmName, RSASignaturePadding rSASignaturePadding, string data) => _RSAUtilBase.SignData(data, hashAlgorithmName, rSASignaturePadding);
+
+        public static bool RSAVerifyData(RSAUtilBase rSAUtilBase, HashAlgorithmName hashAlgorithmName, RSASignaturePadding rSASignaturePadding, string data, string sign) =>
+            _RSAUtilBase.VerifyData(data, sign, hashAlgorithmName, rSASignaturePadding);
 
     }
 }
